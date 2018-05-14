73867e991d90b26e56ddbbafb2d1cb3b63795b32
Carlos Feria <carlosthe19916@gmail.com>
2018-05-14 12:13:59 -0500

    feat(add camel router): add camel sunat router

--- src/main/java/io/github/carlosthe19916/JmsRouter.java
+++ src/main/java/io/github/carlosthe19916/JmsRouter.java
@@ -1,4 +1,30 @@
 package io.github.carlosthe19916;
 
-public class JmsRouter {
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.ContextName;
+import org.apache.camel.component.jms.JmsComponent;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.jms.ConnectionFactory;
+
+@ApplicationScoped
+@ContextName("cdi-camel-context")
+public class JmsRouter extends RouteBuilder {
+
+    @Resource(mappedName = "java:jboss/DefaultJMSConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Override
+    public void configure() throws Exception {
+        JmsComponent jmsComponent = new JmsComponent();
+        jmsComponent.setConnectionFactory(connectionFactory);
+
+        getContext().addComponent("jms", jmsComponent);
+
+        from("jms:queue:SunatQueue")
+                .log("SunatQueue received message: ${body}");
+
+    }
+
 }
