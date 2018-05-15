39ad9e6397ba7a32aac27418e95e7eb17f5435c8
Carlos Feria <carlosthe19916@gmail.com>
2018-05-15 10:52:25 -0500

    feat(refactor jsf views): add templates to views

--- src/main/java/io/github/carlosthe19916/repository/JmsRouter.java
+++ src/main/java/io/github/carlosthe19916/repository/JmsRouter.java
@@ -1,9 +1,13 @@
 package io.github.carlosthe19916.repository;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.cdi.ContextName;
 import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.util.CastUtils;
+import org.apache.cxf.headers.Header;
 import org.wildfly.swarm.spi.runtime.annotations.ConfigurationValue;
 
 import javax.activation.DataHandler;
@@ -13,6 +17,11 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.jms.ConnectionFactory;
 import javax.mail.util.ByteArrayDataSource;
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPHeader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
@@ -37,38 +46,83 @@ public class JmsRouter extends RouteBuilder {
 
         from("jms:queue:SunatQueue")
                 .choice()
-                    .when(header("CamelSunatEndpoint").isNull())
-                        .setHeader("CamelSunatEndpoint").simple(defaultCamelSunatEndpoint)
+                .when(header("CamelSunatEndpoint").isNull())
+                .setHeader("CamelSunatEndpoint").simple(defaultCamelSunatEndpoint)
                 .end()
+                .process((Processor) this::addWSSESecurityHeader)
                 .choice()
-                    .when(body().isInstanceOf(String.class))
-                        .setHeader("CamelSunatOperation").simple("getStatus")
-                        .toD(URI_TEMPLATE)
-                    .when(body().isInstanceOf(byte[].class))
-                        .marshal()
-                        .zip()
-                        .process(exchange -> {
-                            Message message = exchange.getIn();
+                .when(body().isInstanceOf(String.class))
+                .setHeader("CamelSunatOperation").simple("getStatus")
+                .toD(URI_TEMPLATE)
+                .when(body().isInstanceOf(byte[].class))
+                .marshal()
+                .zip()
+                .process(exchange -> {
+                    Message message = exchange.getIn();
 
-                            String ruc = (String) message.getHeader("CamelSunatRuc");
-                            String tipoComprobante = (String) message.getHeader("CamelSunatTipoComprobante");
+                    String ruc = (String) message.getHeader("CamelSunatRuc");
+                    String tipoComprobante = (String) message.getHeader("CamelSunatTipoComprobante");
 
-                            String fileName = (String) message.getHeader("CamelFileName");
-                            byte[] bytes = (byte[]) message.getBody();
-                            String partyType = (String) Optional.ofNullable(message.getHeader("SunatPartyType")).orElse("");
+                    String fileName = (String) message.getHeader("CamelFileName");
+                    byte[] bytes = (byte[]) message.getBody();
+                    String partyType = (String) Optional.ofNullable(message.getHeader("SunatPartyType")).orElse("");
 
-                            DataSource dataSource = new ByteArrayDataSource(bytes, "application/xml");
-                            DataHandler dataHandler = new DataHandler(dataSource);
+                    DataSource dataSource = new ByteArrayDataSource(bytes, "application/xml");
+                    DataHandler dataHandler = new DataHandler(dataSource);
 
-                            Object[] serviceParams = new Object[]{ruc + "-" + tipoComprobante + "-" + fileName.replaceAll(".xml", ".zip"), dataHandler, partyType};
+                    Object[] serviceParams = new Object[]{ruc + "-" + tipoComprobante + "-" + fileName.replaceAll(".xml", ".zip"), dataHandler, partyType};
 
-                            message.setBody(serviceParams);
-                        })
-                        .toD(URI_TEMPLATE)
-                    .otherwise()
-                        .log("SunatQueue received invalid message")
+                    message.setBody(serviceParams);
+                })
+                .toD(URI_TEMPLATE)
+                .otherwise()
+                .log("SunatQueue received invalid message")
                 .end()
         ;
+    }
+
+    public void addWSSESecurityHeader(Exchange exchange) {
+        Message message = exchange.getIn();
+
+        String username = (String) message.getHeader("CamelSunatUsername");
+        String password = (String) message.getHeader("CamelSunatPassword");
+
+        String soapHeader = "" +
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "<wsse:Security xmlns:wsse=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\"\n" +
+                "               xmlns:wsu=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">\n" +
+                "    <wsse:UsernameToken wsu:Id=\"UsernameToken-50\">\n" +
+                "        <wsse:Username>" +
+                username +
+                "        </wsse:Username>\n" +
+                "        <wsse:Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText\">\n" +
+                password +
+                "        </wsse:Password>\n" +
+                "    </wsse:UsernameToken>\n" +
+                "</wsse:Security>";
+
+        //Add wsse security header to the exchange
+        addSoapHeader(exchange, soapHeader);
+    }
+
+    public void addSoapHeader(Exchange exchange, String soapHeader) {
+//        List<Object> soapHeaders = CastUtils.cast((List<?>) exchange.getIn().getHeader(Header.HEADER_LIST));
+//        if (soapHeaders == null) {
+//            soapHeaders = new ArrayList<>();
+//        }
+//
+//        SOAPHeader newHeader;
+//        try {
+//            new SoapHeader
+//            newHeader = new SOAPHeader(new QName("soapHeader"), DOMUtils.readXml(new StringReader(soapHeader)).getDocumentElement());
+//            newHeader.setDirection(Header.Direction.DIRECTION_OUT);
+//
+//            soapHeaders.add(newHeader);
+//
+//            exchange.getIn().setHeader(Header.HEADER_LIST, soapHeaders);
+//        } catch (Exception e) {
+//            //log error
+//        }
     }
 
 }
