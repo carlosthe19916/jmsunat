052617f1dc8519eb62b9df63043deaaa87e5ee65
Carlos Feria <carlosthe19916@gmail.com>
2018-06-08 17:59:58 -0500

    feat(consumer-wildflyswarm): add new module

--- bin/sender.sh
+++ bin/sender.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mvn clean package -DskipTests
-java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5006 boot/sender/target/*-swarm.jar -Dswarm.port.offset=100
+java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5006 distribution/sender/target/*-swarm.jar -Dswarm.port.offset=100
