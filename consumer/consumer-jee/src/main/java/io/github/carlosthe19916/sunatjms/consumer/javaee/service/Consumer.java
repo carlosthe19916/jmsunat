27d4129eb51a603f59d09b93d731b9a3746a474b
Carlos Feria <carlosthe19916@gmail.com>
2018-11-07 17:40:43 -0500

    correct producer and consumer

--- consumer/consumer-jee/src/main/java/io/github/carlosthe19916/sunatjms/consumer/javaee/service/Consumer.java
+++ consumer/consumer-jee/src/main/java/io/github/carlosthe19916/sunatjms/consumer/javaee/service/Consumer.java
@@ -2,6 +2,7 @@ package io.github.carlosthe19916.sunatjms.consumer.javaee.service;
 
 import org.jboss.ejb3.annotation.ResourceAdapter;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
@@ -10,8 +11,8 @@ import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 import java.util.logging.Logger;
 
-@ResourceAdapter("remote-artemis")
-@MessageDriven(name = "HelloWorldQueueMDB", activationConfig = {
+@ResourceAdapter("remote-artemis-connection-factory")
+@MessageDriven(name = "HelloWorldQueueMDB1", activationConfig = {
         @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "java:/exampleQueue"),
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
         @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")
@@ -20,6 +21,7 @@ public class Consumer implements MessageListener {
 
     private static final Logger LOGGER = Logger.getLogger(Consumer.class.toString());
 
+    @Override
     public void onMessage(Message rcvMessage) {
         TextMessage msg = null;
         try {
